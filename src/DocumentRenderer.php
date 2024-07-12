a0b913cc2101679777290b5ded9adfd66e0a4449
tamdaz <tamda.zohir.pro@gmail.com>
2024-07-12 22:00:00 +0200

    Organize methods. Format DocumentRenderer.php for better comprehension.

--- src/DocumentRenderer.php
+++ src/DocumentRenderer.php
@@ -28,6 +28,97 @@ class DocumentRenderer
     }
 
     /**
+     * Render all documentation in specified classes as HTML files.
+     *
+     * @throws DOMException
+     */
+    public function render(): void
+    {
+        $outputPath = $this->getPath() . $this->class->getShortName() . ".html";
+
+        // To avoid that "DOMDocument" class triggers a warning because of HTML5 tags,
+        // an arobase (@) is specified during HTML file is loading.
+        @$this->dom->loadHTMLFile(__DIR__ . '/../templates/empty-doc.html');
+
+        $this->generateListOfClasses();
+        $this->generateDocMethods();
+        $this->generateListOfPropertiesAndMethods();
+
+        $this->dom->saveHTMLFile($outputPath);
+    }
+
+    /**
+     * Get the path where HTML docs are saved.
+     *
+     * @return string
+     */
+    public function getPath(): string
+    {
+        return $this->path;
+    }
+
+    /**
+     * Allows to display all classes in specific classes.
+     * @throws DOMException
+     * @throws Exception
+     */
+    private function generateListOfClasses(): void
+    {
+        global $selectedNamespace;
+
+        $asideLeft = $this->dom->getElementById("asideNamespacesAndClasses");
+
+        $h2 = $this->dom->createElement("h2", "Classes");
+        $asideLeft->appendChild($h2);
+
+        // All classes in specified namespace.
+        $ul = $this->dom->createElement("ul");
+
+        $classesInNamespace = ClassFinder::getClassesInNamespace($selectedNamespace);
+
+        foreach ($classesInNamespace as $class) {
+            $className = last(explode("\\", $class));
+
+            $li = $this->dom->createElement("li");
+            $a = $this->dom->createElement("a", $class);
+            $a->setAttribute("href", $className . ".html");
+
+            $li->appendChild($a);
+            $ul->appendChild($li);
+        }
+
+        $asideLeft->appendChild($ul);
+    }
+
+    /**
+     * Allows to display all information for each method in a specific class.
+     *
+     * @throws DOMException
+     */
+    private function generateDocMethods(): void
+    {
+        $main = $this->dom->getElementById("mainBlock");
+
+        foreach ($this->class->getMethods() as $method) {
+            $docBlock = new DocBlock($method);
+
+            $methodName = $method->getName();
+            $returnType = $method->getReturnType();
+            $divId = "doc2web_method_" . $methodName;
+
+            $div = $this->dom->createElement("div");
+            $div->setAttribute("id", $divId);
+            $h2 = $this->dom->createElement("h1", "$methodName() -> $returnType");
+            $h2->setAttribute("style", "font-family: Ubuntu Sans Mono, monospace;");
+
+            $p = $this->dom->createElement("p", $docBlock->getShortDescription());
+
+            $div->append($h2, $p);
+            $main->appendChild($div);
+        }
+    }
+
+    /**
      * Allows to generate a list of properties and methods in a class.
      *
      * @throws DOMException
@@ -66,90 +157,5 @@ class DocumentRenderer
         }
 
         $asideRight->appendChild($ul);
-    }
-
-    /**
-     * Allows to display all classes in specific classes.
-     * @throws DOMException
-     * @throws Exception
-     */
-    private function generateListOfClasses(): void
-    {
-        global $selectedNamespace;
-
-        $asideLeft = $this->dom->getElementById("asideNamespacesAndClasses");
-
-        $h2 = $this->dom->createElement("h2", "Classes");
-        $asideLeft->appendChild($h2);
-
-        // All classes in specified namespace.
-        $ul = $this->dom->createElement("ul");
-
-        $classesInNamespace = ClassFinder::getClassesInNamespace($selectedNamespace);
-
-        foreach ($classesInNamespace as $class) {
-            $li = $this->dom->createElement("li");
-            $a = $this->dom->createElement("a", $class);
-            $a->setAttribute("href", last(explode("\\", $class)) . ".html");
-
-            $li->appendChild($a);
-            $ul->appendChild($li);
-        }
-
-        $asideLeft->appendChild($ul);
-    }
-
-    /**
-     * Allows to display all information for each method in a specific class.
-     *
-     * @throws DOMException
-     */
-    private function generateDocMethods(): void
-    {
-        $main = $this->dom->getElementById("mainBlock");
-
-        foreach ($this->class->getMethods() as $method) {
-            $docBlock = new DocBlock($method);
-
-            $div = $this->dom->createElement("div");
-            $div->setAttribute("id", "doc2web_method_" . $method->getName());
-            $h2 = $this->dom->createElement("h1", "{$method->getName()}() -> {$method->getReturnType()}");
-            $h2->setAttribute("style", "font-family: Ubuntu Sans Mono, monospace;");
-
-            $p = $this->dom->createElement("p", $docBlock->getShortDescription());
-
-            $div->appendChild($h2);
-            $div->appendChild($p);
-
-            $main->appendChild($div);
-        }
-    }
-
-    /**
-     * Render all documentation in specified classes as HTML files.
-     *
-     * @throws DOMException
-     */
-    public function render(): void
-    {
-        // To avoid that "DOMDocument" class triggers a warning because of HTML5 tags,
-        // an arobase (@) is specified during HTML file is loading.
-        @$this->dom->loadHTMLFile(__DIR__ . '/../templates/empty-doc.html');
-
-        $this->generateListOfClasses();
-        $this->generateDocMethods();
-        $this->generateListOfPropertiesAndMethods();
-
-        $this->dom->saveHTMLFile($this->getPath() . $this->class->getShortName() . ".html");
-    }
-
-    /**
-     * Get the path where HTML docs are saved.
-     *
-     * @return string
-     */
-    public function getPath(): string
-    {
-        return $this->path;
     }
 }
