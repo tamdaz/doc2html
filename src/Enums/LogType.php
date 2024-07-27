52cc631300985ba444f4319d8908724f94afb63f
tamdaz <tamda.zohir.pro@gmail.com>
2024-07-28 00:39:42 +0200

    Add `PROGRESS_COLOR` case in `LogType` enum.

--- src/Enums/LogType.php
+++ src/Enums/LogType.php
@@ -8,12 +8,9 @@ namespace Tamdaz\Doc2Html\Enums;
 enum LogType: string
 {
     case DEFAULT_COLOR = "\e[39m";
-
     case INFO_COLOR = "\e[34m";
-
     case SUCCESS_COLOR = "\e[32m";
-
     case WARNING_COLOR = "\e[33m";
-
     case ERROR_COLOR = "\e[31m";
+    case PROGRESS_COLOR = "\e[35m";
 }
